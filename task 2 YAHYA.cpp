fc66a35f1efa07ae45c90c17f28e930273c65085
yahyaafridi546-arch <yahyaafridi546@gmail.com>
2026-02-17 10:51:43 +0500

    Add files via upload

---
+++ task 2 YAHYA.cpp
@@ -0,0 +1,19 @@
+//display hello world 
+#include <iostream>
+using namespace std;
+int main (){
+	cout <<"hello world"<<endl;
+	cout <<"MUHAMMAD YAHYA AFRIDI"<<endl;
+	cout <<"5124476"<<endl;
+	cout <<"3.0"<<endl;
+	cout <<"5459"<<endl;
+	cout <<"5459"<<endl;
+	cout <<"dear customer the service will be not available today . sorry for inconvenience created"<<endl;
+	cout <<"happy to help!"<<endl;
+	cout <<"great question!"<<endl;
+	cout <<"nice to meet you!"<<endl;
+	cout <<"may i ask why that is ?..."<<endl;
+	cout <<"thanks for bringing this to our attention!"<<endl;
+	cout <<"i completely understand why you'd want that"<<endl;
+	return 0 ;
+}
