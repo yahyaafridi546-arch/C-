bf032cc2e046dfd85951856671bbb32b2ace0fe3
yahyaafridi546-arch <yahyaafridi546@gmail.com>
2026-03-11 08:26:14 +0500

    Add files via upload

---
+++ LAB 1/Lab work/LAB_01 LabWork_1 1.cpp
@@ -0,0 +1,19 @@
+// Name: YAHYA AFRIDI
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0719
+// Section: A
+
+#include <iostream>   // Header file for input and output
+using namespace std;  // Using standard namespace
+
+int main() 
+{
+    // Printing Hello World on the screen
+    cout << "Hello World!";
+
+    return 0;  // Program ends here
+}
+
+
+
+
