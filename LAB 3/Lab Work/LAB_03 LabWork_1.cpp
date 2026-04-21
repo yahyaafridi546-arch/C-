4b8916578777212360da03c7809658f2c2b8a606
yahyaafridi546-arch <yahyaafridi546@gmail.com>
2026-04-21 10:40:44 +0500

    Add files via upload

---
+++ LAB 3/Lab Work/LAB_03 LabWork_1.cpp
@@ -0,0 +1,29 @@
+// Name: maaz wahab
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0719
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;  // variables to store numbers
+
+    cout << "Enter the first number: ";
+    cin >> a;  // read first number
+
+    cout << "Enter the second number: ";
+    cin >> b;  // read second number
+
+    if (a == b)  // check equality
+        cout << "Both numbers are equal" << endl;
+    if (a > b)   // check if first is greater
+        cout << "First number is greater than second" << endl;
+    if (a < b)   // check if first is smaller
+        cout << "First number is less than second" << endl;
+
+    return 0;  // program ends
+}
+
+
